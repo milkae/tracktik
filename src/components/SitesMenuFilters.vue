0e6b738781c0b055feb0659bd346443a23662451
Floriane <floriane@tuta.io>
2020-01-29 16:42:04 -0500

    Styles sites list select

--- src/components/SitesMenuFilters.vue
+++ src/components/SitesMenuFilters.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="sites-menu__filters">
     <select v-if="clients.length" v-model="client">
       <option value="">All sites</option>
       <option v-for="client in clients" :key="client.id" :value="client.id">{{
@@ -33,3 +33,36 @@ export default class SitesMenuFilters extends Vue {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.sites-menu__filters {
+  position: relative;
+  display: flex;
+  height: 3em;
+  line-height: 3;
+  overflow: hidden;
+
+  &::after {
+    content: "\25BC";
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 0 1em;
+    cursor: pointer;
+    pointer-events: none;
+  }
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    appearance: none;
+    outline: 0;
+    box-shadow: none;
+    border: 0;
+    background-image: none;
+    flex: 1;
+    padding: 0 0.5em;
+  }
+}
+</style>
